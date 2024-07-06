3b3fc0aa63f4c796ef27424901cfd173fa133100
Anthony <91128594+yellowthing0@users.noreply.github.com>
2024-07-06 13:58:00 +1000

    Darkmode

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace PhotoViewerApp;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace PhotoViewerApp
 {
+    public partial class App : Application
+    {
+    }
 }
-
